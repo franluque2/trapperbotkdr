522a5ef60fd974a7d1ae8094d2ca110e243849ed
Fran Luque <franluque2@gmail.com>
2023-01-23 23:18:39 -0300

    small change

--- src/events/events/buildtrap.js
+++ src/events/events/buildtrap.js
@@ -81,6 +81,7 @@ module.exports = async (client, interaction) => {
             },
           };
           const result = await trappers.updateOne(filter, updateplayerstatus);
+          await interaction.update({components:[]})
           return
         }
       }
